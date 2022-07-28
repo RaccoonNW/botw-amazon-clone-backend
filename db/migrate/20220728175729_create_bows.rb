2ccc61a124b647485d70ac081bfa731e60c4d2a4
Gabriel <shalitmontagne.g@gmail.com>
2022-07-28 11:57:48 -0600

    Add shields model

---
+++ db/migrate/20220728175729_create_bows.rb
@@ -0,0 +1,13 @@
+class CreateBows < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bows do |t|
+      t.string :name
+      t.integer :attack
+      t.integer :durability
+      t.integer :range
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end

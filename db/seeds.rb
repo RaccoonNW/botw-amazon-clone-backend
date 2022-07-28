b67158c6d41ab45e50571a4121688c5bb712f21a
Gabriel <shalitmontagne.g@gmail.com>
2022-07-28 16:52:54 -0600

    Layout scraping data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,20 @@
-weaponsPage = HTTParty.get('https://zelda.fandom.com/wiki/Weapon#Breath_of_the_Wild')
-html = Nokogiri::HTML(weaponsPage)
+bowPage = HTTParty.get('https://zelda.fandom.com/wiki/Bow#Breath_of_the_Wild')
+html = Nokogiri::HTML(bowPage)
 tables = html.css('table')
-tableRow = tables[0].css('tr')
+tableRow = tables[9].css('tr')
 
-puts tableRow[1].css('td')[7].inner_text
+puts tableRow[1].css('td')[0].inner_text
 # .children.children[0].attributes['data-src']
+
+
+# WEAPONS --------
+
+# weaponsPage = HTTParty.get('https://zelda.fandom.com/wiki/Weapon#Breath_of_the_Wild')
+# html = Nokogiri::HTML(weaponsPage)
+# tables = html.css('table')
+# tableRow = tables[0].css('tr')
+
+# puts tableRow[1].css('td')[7].inner_text
 
 # CHANGE tableRow KEY TO CHANGE WEAPON
 # Weapon Name:
@@ -24,3 +34,33 @@ puts tableRow[1].css('td')[7].inner_text
 
 # Weapon Description:
 # tableRow[1].css('td')[7].inner_text
+
+# ARMOR ---------------------------------
+
+# armorPage = HTTParty.get('https://zelda.fandom.com/wiki/Armor')
+# html = Nokogiri::HTML(armorPage)
+# tables = html.css('table')
+# tableRow = tables[0].css('tr')
+
+# WORKS FOR ALL ROWS - CHANGE KEY TO CHANGE ITEM/COLUMN
+# puts tableRow[1].css('td')[0].inner_text
+
+# SHIELDS ----------------------------------
+
+# shieldPage = HTTParty.get('https://zelda.fandom.com/wiki/Shield#Breath_of_the_Wild')
+# html = Nokogiri::HTML(shieldPage)
+# tables = html.css('table')
+# tableRow = tables[3].css('tr')
+
+# puts tableRow[1].css('td')[0].inner_text
+
+# BOWS ---------------------------------------
+
+# bowPage = HTTParty.get('https://zelda.fandom.com/wiki/Bow#Breath_of_the_Wild')
+# html = Nokogiri::HTML(bowPage)
+# tables = html.css('table')
+# tableRow = tables[9].css('tr')
+
+# puts tableRow[1].css('td')[0].inner_text
+
+# ARROWS -------------------------------------------

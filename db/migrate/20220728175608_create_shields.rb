9e5861a16bd23a816eb978cb97c94b0fd59bc4af
Gabriel <shalitmontagne.g@gmail.com>
2022-07-28 11:56:42 -0600

    Create shields model

---
+++ db/migrate/20220728175608_create_shields.rb
@@ -0,0 +1,14 @@
+class CreateShields < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shields do |t|
+      t.string :name
+      t.integer :guard
+      t.integer :durability
+      t.string :material
+      t.string :image
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
